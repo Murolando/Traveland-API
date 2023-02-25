80d978e1fd9e08eb52deb38b5dc37c9765f04dbb
Murolando <60064242+Murolando@users.noreply.github.com>
2023-02-25 16:32:44 +0300

    New Migrations

--- db/migrations/000001_app_bd.up.sql
+++ db/migrations/000001_app_bd.up.sql
@@ -27,7 +27,7 @@ CREATE TABLE "house_type"
     id                      serial PRIMARY KEY not null unique,
     name                    varchar(100) not null 
 );
-INSERT INTO "house_type" (name) values('hotel'); 
+INSERT INTO "house_type" (name) values('Отель'); 
 
 CREATE TABLE "place"
 (
@@ -52,19 +52,6 @@ CREATE TABLE "place"
     count_room              INT DEFAULT 0, 
     square                  INT DEFAULT 0
 );
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,mail,site_url,house_price,house_type_id) values('vld','vladik alooo',43.02708839915204,44.67459994750738,'jil','8928','jilnya','ublya',14500,1);
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,mail,site_url,house_price,house_type_id) values('alex','vladik alooo',43.03088949618408,44.6801429109748,'jil','8928','jilnya','ublya',5400,1);
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,mail,site_url,house_price,house_type_id) values('dom','vladik alooo',43.03988710538061,44.668457276358524,'jil','8928','jilnya','ublya',150,1);
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,mail,site_url,house_price,house_type_id) values('dom2','vladik alooo',43.03842660342706,44.62910925868951,'jil','8928','jilnya','ublya',2400,1);
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,mail,site_url,house_price,house_type_id) values('geetto','vladik alooo',43.00996804849331,44.67571994298377,'jil','8928','jilnya','ublya',1300,1);
-
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,pushkin,min_price) values('place1','jil',0.0,0.0,'jil','8928',TRUE,2500);  
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,pushkin,min_price) values('event','jil',0.0,0.0,'jil','8928',TRUE,350);
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,pushkin,min_price,event_day,event_start_time) values('event2','jil',0.0,0.0,'jil','8928',TRUE,350,'2023-12-22','10:24:15');
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,pushkin,min_price,event_day,event_start_time) values('event3','jil',0.0,0.0,'jil','8928',TRUE,350,'2023-12-22','12:24:00');
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,pushkin,min_price,event_day,event_start_time) values('event4','jil',0.0,0.0,'jil','8928',TRUE,350,'2023-12-22','12:24:00');
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,pushkin,min_price,event_day,event_start_time) values('event5','jil',0.0,0.0,'jil','8928',TRUE,350,'2023-12-22','12:24:00');
-INSERT INTO "place" (name,description,location_long,location_lat,address,numbers,pushkin,min_price,event_day,event_start_time) values('event6`','jil',0.0,0.0,'jil','8928',TRUE,350,'2023-12-22','12:24:00');
 
 CREATE TABLE "place_src"
 (
@@ -93,31 +80,15 @@ CREATE TABLE "type"
     id                      serial  PRIMARY KEY  not null unique,
     name                    varchar(100) not null
 );
-INSERT INTO "type" (name) values('housing'); 
-INSERT INTO "type" (name) values('event'); 
-INSERT INTO "type" (name) values('cafe'); 
+INSERT INTO "type" (name) values('Жильё'); 
+INSERT INTO "type" (name) values('Событие'); 
+INSERT INTO "type" (name) values('Ресторан'); 
 CREATE TABLE "place_type"
 (
     id                      serial PRIMARY KEY not null unique,
     place_id                int references "place" (id) on delete cascade not null,
     type_id                 int references "type" (id) on delete cascade not null
-);
-INSERT INTO "place_type" (place_id,type_id) values(1,1); 
-INSERT INTO "place_type" (place_id,type_id) values(2,1); 
-INSERT INTO "place_type" (place_id,type_id) values(3,1); 
-INSERT INTO "place_type" (place_id,type_id) values(4,1); 
-INSERT INTO "place_type" (place_id,type_id) values(5,1);
-
-INSERT INTO "place_type" (place_id,type_id) values(6,3); 
-
-INSERT INTO "place_type" (place_id,type_id) values(7,2); 
-INSERT INTO "place_type" (place_id,type_id) values(8,2); 
-INSERT INTO "place_type" (place_id,type_id) values(9,2); 
-INSERT INTO "place_type" (place_id,type_id) values(10,2); 
-INSERT INTO "place_type" (place_id,type_id) values(11,2);
-INSERT INTO "place_type" (place_id,type_id) values(12,2);
- 
-
+); 
 
 CREATE TABLE "achieve"
 (
@@ -134,30 +105,27 @@ CREATE TABLE "user_achieve"
 CREATE TABLE "dayy"
 (
     id                      serial PRIMARY KEY not null unique,
-    name                    varchar(14) not null
+    name                    varchar(15) not null
 );
-INSERT INTO "dayy" (name) values('Воскресенье'); 
 INSERT INTO "dayy" (name) values('Понедельник'); 
 INSERT INTO "dayy" (name) values('Вторник'); 
 INSERT INTO "dayy" (name) values('Среда'); 
 INSERT INTO "dayy" (name) values('Четверг'); 
 INSERT INTO "dayy" (name) values('Пятница'); 
-INSERT INTO "dayy" (name) values('Суббота'); 
-
+INSERT INTO "dayy" (name) values('Суббота');
+INSERT INTO "dayy" (name) values('Воскресенье'); 
 
 CREATE TABLE "week"
 (
     id                      serial PRIMARY KEY  not null unique,
     place_id                int references "place" (id) on delete cascade not null,
-    day_id                  int references "dayy"(id) on delete cascade not null unique,
+    day_id                  int references "dayy"(id) on delete cascade not null,
     start_work              time,
     end_work                time,
     start_timeout           time,
     end_timeout             time 
 );
 
-INSERT INTO "week" (place_id,day_id,start_work,end_work) values(1,2,'12:00:00','16:00:00'); 
-INSERT INTO "week" (place_id,day_id,start_work,end_work) values(1,3,'12:00:00','16:00:00'); 
 CREATE TABLE "tour"
 (
     id                      serial PRIMARY KEY  not null unique,
@@ -193,7 +161,3 @@ CREATE TABLE "banner_place"
     order_number            int not null,
     image_src               varchar(500) not null
 );
-
-
-INSERT INTO "banner_place" (place_id,banner_id,order_number,image_src) values(3,1,2,'oleg.jpg'); 
-INSERT INTO "banner_place" (place_id,banner_id,order_number,image_src) values(1,1,1,'oleg.jpg'); 
